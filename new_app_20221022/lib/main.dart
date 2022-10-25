15f9e5f8fa27b0545e8ff896fd15f69dbf96dc27
wlsdnr929 <wlsdnr929@naver.com>
2022-10-25 19:09:00 +0900

    뉴스 앱

--- new_app_20221022/lib/main.dart
+++ new_app_20221022/lib/main.dart
@@ -1,6 +1,8 @@
+// 뉴스 앱
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,35 +13,115 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
+          foregroundColor: Colors.pink,
+          titleTextStyle: TextStyle(fontSize: 24, color: Colors.pink),
           centerTitle: false,
-          titleTextStyle: TextStyle(
-            color: Colors.pink,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          title: Text(
-            '테디피셜',
-          ),
           elevation: 0,
+          title: Text('테디피셜'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.settings,
+                size: 32,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://picsum.photos/200'),
+              ),
+            )
+          ],
         ),
         body: ListView(
           children: [
             ListTile(
-              title: Text(
-                '공지사항',
-                style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold),
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
               ),
-              subtitle: Text('반갑습니다 여러분'),
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.announcement),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
               ),
-              trailing: Icon(Icons.navigate_next),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
+            ),
+            ListTile(
+              title: Text('테디피셜 제공'),
+              subtitle: Text(
+                '모기에 물리면 간지러운 사실 알고계셨나요?',
+                style: TextStyle(fontSize: 22),
+              ),
+              trailing: Image.network('https://picsum.photos/200'),
             ),
           ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: 'Favorite'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'My'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.pink,
+          onPressed: () {}, //
+          child: Icon(Icons.edit),
         ),
       ),
     );
